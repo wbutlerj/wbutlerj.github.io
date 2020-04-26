6428db79c273516f215a69f0731a40c6f34b2e83
Use your GitHub username <Enter your email address>
2020-04-25 22:17:55 -0600

    update

---
+++ dateModified.js
@@ -0,0 +1,6 @@
+const options = {year:'numeric'};
+const options1 = {day:'numeric', month:'long', year:'numeric', hour:'numeric', minute:'numeric'};
+document.getElementById("mdate").innerHTML = new Date(document.lastModified).toLocaleDateString('en-US', options1);
+document.getElementById("year").innerHTML = new Date().toLocaleDateString('en-US', options);
+
+

fada1a383d61e899e4a6d1828bab58d7eb396b1c
Use your GitHub username <Enter your email address>
2020-05-07 00:29:42 -0600

    update

--- JS/dateModified.js
+++ JS/dateModified.js
@@ -1,6 +1,12 @@
-const options = {year:'numeric'};
-const options1 = {day:'numeric', month:'long', year:'numeric', hour:'numeric', minute:'numeric'};
+const options = {
+    year: 'numeric'
+};
+const options1 = {
+    day: 'numeric',
+    month: 'long',
+    year: 'numeric',
+    hour: 'numeric',
+    minute: 'numeric'
+};
 document.getElementById("mdate").innerHTML = new Date(document.lastModified).toLocaleDateString('en-US', options1);
 document.getElementById("year").innerHTML = new Date().toLocaleDateString('en-US', options);
-
-

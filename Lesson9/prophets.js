eaa5fa57b4108d419e5bc759c79758596324fb0f
Use your GitHub username <Enter your email address>
2020-06-17 20:35:45 -0600

    initialize prophets

---
+++ Lesson9/prophets.js
@@ -0,0 +1,40 @@
+const requestURL = 'https://byui-cit230.github.io/lessons/lesson-09/data/latter-day-prophets.json';
+
+
+fetch(requestURL)
+  .then(function (response) {
+    return response.json();
+  })
+  .then(function (jsonObject) {
+    console.table(jsonObject);
+    const prophets = jsonObject['prophets'];  // temporary checking for valid response and data parsing
+    for (let i = 0; i < prophets.length; i++ ) {
+        let card = document.createElement('section');
+        let h2 = document.createElement('h2');
+        h2.textContent = prophets[i].name + ' ' + prophets[i].lastname;
+        card.appendChild(h2);
+        document.querySelector('div.cards').appendChild(card);
+
+        let birthdate = document.createElement('p');
+        birthdate.textContent = "Date of Birth: " + prophets[i].birthdate;
+        card.appendChild(birthdate);
+
+        let birthplace = document.createElement('p');
+        birthplace.textContent = "Place of Birth: " + prophets[i].birthplace;
+        card.appendChild(birthplace);
+
+        let image = document.createElement('img');
+        image.setAttribute('src', prophets[i].imageurl);
+        card.appendChild(image);
+      
+      
+
+    }
+  });
+
+  
+
+ 
+ 
+
+//   image.setAttribute('src', prophets[i].imageurl);
